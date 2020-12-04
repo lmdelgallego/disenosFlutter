db6314222aa03e207af4c9edef9a3f570b31263b
Luis Miguel Del Gallego <alucardluis@gmail.com>
2020-12-04 08:11:15 -0500

    finish dashboard

--- lib/src/pages/dashboard_page.dart
+++ lib/src/pages/dashboard_page.dart
@@ -1,4 +1,5 @@
 import 'dart:math';
+import 'dart:ui';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -19,6 +20,7 @@ class DashboardPage extends StatelessWidget {
             child: Column(
               children: [
                 _titleContent(),
+                _buttonsGrid(),
               ],
             ),
           )
@@ -28,7 +30,7 @@ class DashboardPage extends StatelessWidget {
     );
   }
 
-  BottomNavigationBar _buildBottomNavigationBar() {
+  Widget _buildBottomNavigationBar() {
     return BottomNavigationBar(
       backgroundColor: Color.fromRGBO(55, 57, 84, 1.0),
       selectedItemColor: Colors.pinkAccent,
@@ -81,6 +83,13 @@ class DashboardPage extends StatelessWidget {
         height: 360.0,
         width: 360.0,
         decoration: BoxDecoration(
+          boxShadow: [
+            BoxShadow(
+              blurRadius: 30,
+              color: Color.fromRGBO(236, 98, 188, 1.0),
+              spreadRadius: 10,
+            ),
+          ],
           borderRadius: BorderRadius.circular(80.0),
           gradient: LinearGradient(
             colors: [
@@ -127,6 +136,85 @@ class DashboardPage extends StatelessWidget {
               ),
             )
           ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buttonsGrid() {
+    return Table(
+      children: [
+        TableRow(
+          children: [
+            _customButtomItem(Colors.blue, Icons.border_all, 'General'),
+            _customButtomItem(
+                Colors.purpleAccent, Icons.directions_bus_outlined, 'Bus'),
+          ],
+        ),
+        TableRow(
+          children: [
+            _customButtomItem(Colors.pinkAccent, Icons.share, 'Buy'),
+            _customButtomItem(Colors.orange, Icons.insert_drive_file, 'file'),
+          ],
+        ),
+        TableRow(
+          children: [
+            _customButtomItem(
+                Colors.blueAccent, Icons.movie_outlined, 'Enterteiment'),
+            _customButtomItem(Colors.green, Icons.cloud_outlined, 'Grocery'),
+          ],
+        ),
+        TableRow(
+          children: [
+            _customButtomItem(Colors.red, Icons.collections, 'Photos'),
+            _customButtomItem(Colors.tealAccent, Icons.help_outline, 'Help'),
+          ],
+        ),
+        TableRow(
+          children: [
+            _customButtomItem(
+                Colors.blueGrey, Icons.settings_outlined, 'Settings'),
+            _customButtomItem(Colors.lime, Icons.share_outlined, 'Shared'),
+          ],
+        ),
+      ],
+    );
+  }
+
+  Widget _customButtomItem(Color color, IconData icon, String label) {
+    return Container(
+      margin: EdgeInsets.all(10),
+      child: ClipRRect(
+        clipBehavior: Clip.antiAlias,
+        borderRadius: BorderRadius.circular(20),
+        child: BackdropFilter(
+          filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
+          child: Container(
+            height: 180,
+            padding: EdgeInsets.all(30),
+            decoration: BoxDecoration(
+              color: Color.fromRGBO(62, 66, 106, 0.7),
+              borderRadius: BorderRadius.circular(20),
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                CircleAvatar(
+                  radius: 35,
+                  backgroundColor: color,
+                  child: Icon(
+                    icon,
+                    color: Colors.white,
+                    size: 30,
+                  ),
+                ),
+                Text(
+                  label,
+                  style: TextStyle(color: color),
+                )
+              ],
+            ),
+          ),
         ),
       ),
     );
